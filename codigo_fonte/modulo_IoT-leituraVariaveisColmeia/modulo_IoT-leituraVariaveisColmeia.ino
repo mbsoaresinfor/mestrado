58d0c70a8ef645d64f5b0c07087e5d6dc21bd11e
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-09-14 19:23:08 -0300

    alteracao leitura do sensor gaz e ajustes em logs.

--- codigo_fonte/modulo_IoT-leituraVariaveisColmeia/modulo_IoT-leituraVariaveisColmeia.ino
+++ codigo_fonte/modulo_IoT-leituraVariaveisColmeia/modulo_IoT-leituraVariaveisColmeia.ino
@@ -27,10 +27,12 @@ File myFile;
 unsigned long contadorAbelha = 0;
 unsigned long contadorLoopParaLeitura = 0;
 int TEMPO_DELAY = 100;
-unsigned long UM_SEGUNDO = ((TEMPO_DELAY * 10)/ 1000);
-unsigned long CINCO_MINUTO = (UM_SEGUNDO * 60 * 5) ; 
+unsigned long UM_SEGUNDO = ((TEMPO_DELAY / 10));
+unsigned long UM_MINUTO = (UM_SEGUNDO * 60) ; 
+unsigned long CINCO_MINUTO = (UM_MINUTO * 5) ; 
 bool s_high=0;
 int maiorSom = 0;
+int maiorGas = 0;
 
 
 
@@ -49,6 +51,8 @@ void setup() {
   Serial.println("Valores dos contadores de tempo");
   Serial.print("UM_SEGUNDO: ");
   Serial.println(UM_SEGUNDO);  
+  Serial.print("UM_MINUTO: ");
+  Serial.println(UM_MINUTO);  
   Serial.print("CINCO_MINUTO: ");
   Serial.println(CINCO_MINUTO);  
   Serial.println("Setup config OK");  
@@ -59,19 +63,41 @@ void setup() {
 // TODO tirar todos os print de log, 
 void loop() {
 
-  if(contadorLoopParaLeitura == CINCO_MINUTO){
+  if(eUmMinuto()){
+    Serial.println("processando operacoes de cada 1 minuto");
+    processaMaiorGas();
+  }
+
+  if(eCincoMinuto()){
+      Serial.println("processando operacoes de cada 5 minutos");
       processaLeituraEscritaSensores();
       contadorLoopParaLeitura = 0;
       maiorSom = 0;
-  }else{
-     contadorLoopParaLeitura++;
-     processaMaiorSomAbelhas();
+      maiorGas = 0;
   }
   
+  processaMaiorSomAbelhas();
   processaContadorAbelhas();  
   
   delay(TEMPO_DELAY);
+  contadorLoopParaLeitura++;  
  
+}
+boolean eCincoMinuto(){
+  return contadorLoopParaLeitura == CINCO_MINUTO;
+}
+boolean eUmMinuto(){
+  return ((contadorLoopParaLeitura % UM_MINUTO) == 0) && contadorLoopParaLeitura != 0;
+}
+
+// armazena o maior gas lido
+void processaMaiorGas(){
+  int  gasLido = analogRead(pinoGas);   
+  if(gasLido > maiorGas){
+    maiorGas = gasLido;
+    Serial.print("setado maior gas: ");     
+    Serial.println(maiorGas);
+  }
 }
 
 // armazena o maior som lido das abelhas.
@@ -80,8 +106,8 @@ void processaMaiorSomAbelhas(){
   int somLido = analogRead(pinoSensorSom);   
   if(somLido > maiorSom){
     maiorSom = somLido;
-     Serial.print("setado maior som: ");
-     Serial.println(maiorSom);
+    Serial.print("setado maior som: ");     
+    Serial.println(maiorSom);
   }
 }
 
@@ -99,7 +125,7 @@ void processaContadorAbelhas(){
 }
 
 void processaLeituraEscritaSensores(){
-    int  gas = analogRead(pinoGas);   
+    
     // parte de leitura de temperatura e humidade.
     sensors_event_t event;                      
     dht.temperature().getEvent(&event);           
@@ -107,21 +133,31 @@ void processaLeituraEscritaSensores(){
     dht.humidity().getEvent(&event);    
     float humidade= event.relative_humidity;        
     
-    String valores = criaStringValoresSensores(contadorAbelha,gas,temperatura,humidade,maiorSom);
+    String valores = criaStringValoresSensores(contadorAbelha,maiorGas,temperatura,humidade,maiorSom);
 
     escreveCartao(valores);
-    Serial.println("processaLeituraEscritaSensores: " );
-    Serial.println("contadorAbelha,gas,temperatura,humidade,maiorSom");
-    Serial.println(valores);
+    Serial.println("processaLeituraEscritaSensores: " );    
+    Serial.print("contadorAbelha: ");
+    Serial.print(contadorAbelha);
+    Serial.print(" - gas: ");
+    Serial.print(maiorGas);
+    Serial.print(" - temperatura: ");
+    Serial.print(temperatura);
+    Serial.print(" - humidade: ");
+    Serial.print(humidade);
+    Serial.print(" - maiorSom:");
+    Serial.print(maiorSom);
+    Serial.println("");
+    
 }
 
 // valores: contador;gas
 String criaStringValoresSensores(int contador,int gas,float temperatura, float humidade,int som ){
   String token = ";";
   String message = "";
-  message.concat(String(gas));    
-  message.concat(String(token));    
   message.concat(String(contador));
+  message.concat(String(token));    
+  message.concat(String(gas));    
   message.concat(String(token));          
   message.concat(String(temperatura));          
   message.concat(String(token));          
