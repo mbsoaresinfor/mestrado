81c7c0cf737aa28fbbcca9b9114d2d47fb1db349
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-11-08 22:58:55 -0300

    melhorias

---
+++ codigo_fonte/componente-back-end/src/main/java/br/com/componente/entidades/DadosApiarioRequisicao.java
@@ -0,0 +1,77 @@
+package br.com.componente.entidades;
+
+
+public class DadosApiarioRequisicao {
+
+
+	private Float tenantId;
+	
+	private Float contadorAnaliseAtividadeEnxame;
+	
+	private Float gas;
+	
+	private Float temperatura;
+
+	private Float umidade;
+	
+	private Float som;
+	
+	private Float peso;
+
+	public Float getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(Float tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public Float getContadorAnaliseAtividadeEnxame() {
+		return contadorAnaliseAtividadeEnxame;
+	}
+
+	public void setContadorAnaliseAtividadeEnxame(Float contadorAnaliseAtividadeEnxame) {
+		this.contadorAnaliseAtividadeEnxame = contadorAnaliseAtividadeEnxame;
+	}
+
+	public Float getGas() {
+		return gas;
+	}
+
+	public void setGas(Float gas) {
+		this.gas = gas;
+	}
+
+	public Float getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(Float temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public Float getUmidade() {
+		return umidade;
+	}
+
+	public void setUmidade(Float umidade) {
+		this.umidade = umidade;
+	}
+
+	public Float getSom() {
+		return som;
+	}
+
+	public void setSom(Float som) {
+		this.som = som;
+	}
+
+	public Float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
+	
+}
